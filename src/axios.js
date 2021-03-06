45c0f7b8a59ea73c988a328fbbaf487f9f2f9206
Mansoor shakir <mansoorshakir642@gmail.com>
2021-03-06 02:04:38 -0800

    updated

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axios_instance = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: process.env.REACT_APP_API,
+  baseURL: process.env.REACT_APP_API,
 });
 
 export default axios_instance;
